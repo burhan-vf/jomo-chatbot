1988f177d5b37eea4a063fccebdebde83b48b8fc
burhan-vf <137755622+burhan-vf@users.noreply.github.com>
2023-06-26 11:57:59 +0500

    updated response and added exceptions

--- get_recommendations.py
+++ get_recommendations.py
@@ -4,6 +4,7 @@ from langchain.vectorstores import Pinecone
 import os
 from dotenv import load_dotenv
 import re
+import json
 
 load_dotenv()
 
@@ -30,12 +31,26 @@ index_name = PINECONE_INDEX_NAME
 docsearch = Pinecone.from_existing_index(index_name, embeddings)
 
 def get_recommendations(query):
-    query = query
-    docs = docsearch.similarity_search(query, k = 30)
+    if query:
+        
+        try:
+            query = query
+            docs = docsearch.similarity_search(query, k = 30)
 
-    recommendations = []
-    for doc in docs:
-        prod_id = re.findall(r'id:\s*\s*(\w+)\s*.*title:\s*\s*', doc.page_content)
-        recommendations.append(prod_id[0])
+            recommendations = []
 
-    return recommendations
+            for doc in docs:
+                prod_id = re.findall(r'id:\s*\s*(\w+)\s*.*title:\s*\s*', doc.page_content)
+                recommendations.append(prod_id[0])
+
+            response = {"response": recommendations}
+            
+            return response
+        
+        except:
+            response = {"response": "No Item Found"}
+            return response
+    
+    else:
+        response = {"response": "Enter a Valid Search Query"}
+        return response
