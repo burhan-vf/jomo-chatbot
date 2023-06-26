0d19c3be2bc30fd257e52f61c037aad1cc3b173c
burhan-vf <137755622+burhan-vf@users.noreply.github.com>
2023-06-26 11:06:24 +0500

    Add files via upload

---
+++ get_recommendations.py
@@ -0,0 +1,41 @@
+from langchain.embeddings.openai import OpenAIEmbeddings
+import pinecone
+from langchain.vectorstores import Pinecone
+import os
+from dotenv import load_dotenv
+import re
+
+load_dotenv()
+
+MODEL_NAME = os.getenv("MODEL_NAME")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENV = os.getenv("PINECONE_ENV")
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+
+
+embeddings = OpenAIEmbeddings(
+    model=MODEL_NAME,
+    openai_api_key=OPENAI_API_KEY
+)
+
+# initialize pinecone
+pinecone.init(
+    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+    environment=PINECONE_ENV,  # next to api key in console
+)
+
+index_name = PINECONE_INDEX_NAME
+
+docsearch = Pinecone.from_existing_index(index_name, embeddings)
+
+def get_recommendations(query):
+    query = query
+    docs = docsearch.similarity_search(query, k = 30)
+
+    recommendations = []
+    for doc in docs:
+        prod_id = re.findall(r'id:\s*\s*(\w+)\s*.*title:\s*\s*', doc.page_content)
+        recommendations.append(prod_id[0])
+
+    return recommendations
---
+++ jomo_api.py
@@ -0,0 +1,17 @@
+from get_recommendations import get_recommendations
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/jomo-recommendations', methods=['POST'])
+def testing():
+    request_json = request.json
+    query = request_json['query']
+
+    recommendations = get_recommendations(query = query)
+    return recommendations
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
